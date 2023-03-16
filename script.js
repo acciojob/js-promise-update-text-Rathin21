26c92aa4d7ff052a02a0192cd6bba939835cdc13
Rathin21 <114067851+Rathin21@users.noreply.github.com>
2023-03-16 12:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const output = document.querySelector("#output");
+
+function func() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	})
+}
+
+func().then((message) => {
+  output.textContent = message;
+});
